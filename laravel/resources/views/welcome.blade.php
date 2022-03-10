1fa2da03b01498f5c5eb896ce0cb3b68825ddf34
Hafida20-bo <bouchachia.hafida.com>
2022-03-10 20:11:35 +0100

    titre

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel 22</title>
+        <title>CPA BANK</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                   
+
                                 </div>
                             </div>
                         </div>
@@ -68,7 +68,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                   
+
                                 </div>
                             </div>
                         </div>
