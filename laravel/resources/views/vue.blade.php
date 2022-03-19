613246a1b03b47b7b345743bbcf2b4712d3814f8
FifiTta20 <feryelbklhacene2000@gmail.com>
2022-03-19 12:00:49 +0100

    change

--- laravel/resources/views/vue.blade.php
+++ laravel/resources/views/vue.blade.php
@@ -1,33 +1,85 @@
+
 <!DOCTYPE html>
 <html >
 <head>
   <meta charset="UTF-8">
-  <title>Formulaire</title>
+  <title>Fiche de client</title>
   
   <link rel="stylesheet" href="css/reset.min.css">
   <link rel="icon" type="image/png" href="{{URL::to('/images/icons/bank-logo.jpg')}}">
   
   <link rel="stylesheet" href="css/styleFormulaire.css">
 
-  
 </head>
 
 <body>
+
   <!-- multistep form -->
 <form id="msform">
   <!-- progressbar -->
   <ul id="progressbar">
-    <li class="active">Account Setup</li>
-    <li>Social Profiles</li>
-    <li>Personal Details</li>
+    <li class="active">Identification</li>
+    <li>Informations</li>
+    <li>Titulaire</li>
+    <li>Conjoint</li>
+    <li>Services Annexes</li>
+    <li>Memo</li>
   </ul>
   <!-- fieldsets -->
   <fieldset>
-    <h2 class="fs-title">Create your account</h2>
-    <h3 class="fs-subtitle">This is step 1</h3> 
-    <input type="text" name="email" placeholder="Email" />
-    <input type="password" name="pass" placeholder="Password" />
-    <input type="password" name="cpass" placeholder="Confirm Password" />
+    <h2 class="fs-title">Identification</h2>
+    <!--h3 class="fs-subtitle">This is step 1</h3--> 
+    
+    <p> <label>Numéro: </label>  <input type="text" /> </p>
+    <p> <label>Agence: </label>  <input type="text" /> </p>
+    <p> <label>Nom: </label>     <input type="text" /> </p>
+    <p> <label>Prénom: </label>   <input type="text" /> </p>
+    
+
+    </br> <h2 class="fs-title">Pièces d'identités</h2>
+    <p> <label>Type: </label>  <input type="text" /> </p>
+    <p> <label>Numéro: </label>  <input type="text" />
+        <label>Validité: </label>     <input type="text" />  </p>
+    <p> <label>Délivrée à: </label>     <input type="text" /> 
+        <label>Le: </label>     <input type="text" /> </p>
+    <p> <label>Prénom: </label>   <input type="text" /> </p>
+     
+
+</br>
+    <input type="button" name="next" class="next action-button" value="Next" /> </br>
+  </fieldset>
+
+
+  <fieldset>
+    <h2 class="fs-title">Social Profiles</h2>
+    <h3 class="fs-subtitle">Your presence on the social network</h3>
+    <input type="text" name="twitter" placeholder="Twitter" />
+    <input type="text" name="facebook" placeholder="Facebook" />
+    <input type="text" name="gplus" placeholder="Google Plus" />
+
+    <label>date: </label>    <input type="date" />   </p>
+    <p> <label>Age: </label>     <select>  <option>Choose</option>  </select> </p>
+
+
+    <input type="button" name="previous" class="previous action-button" value="Previous" />
+    <input type="button" name="next" class="next action-button" value="Next" />
+  </fieldset>
+  <fieldset>
+    <h2 class="fs-title">Social Profiles</h2>
+    <h3 class="fs-subtitle">Your presence on the social network</h3>
+    <input type="text" name="twitter" placeholder="Twitter" />
+    <input type="text" name="facebook" placeholder="Facebook" />
+    <input type="text" name="gplus" placeholder="Google Plus" />
+    <input type="button" name="previous" class="previous action-button" value="Previous" />
+    <input type="button" name="next" class="next action-button" value="Next" />
+  </fieldset>
+  <fieldset>
+    <h2 class="fs-title">Social Profiles</h2>
+    <h3 class="fs-subtitle">Your presence on the social network</h3>
+    <input type="text" name="twitter" placeholder="Twitter" />
+    <input type="text" name="facebook" placeholder="Facebook" />
+    <input type="text" name="gplus" placeholder="Google Plus" />
+    <input type="button" name="previous" class="previous action-button" value="Previous" />
     <input type="button" name="next" class="next action-button" value="Next" />
   </fieldset>
   <fieldset>
@@ -49,8 +101,11 @@
     <input type="button" name="previous" class="previous action-button" value="Previous" />
     <input type="submit" name="submit" class="submit action-button" value="Submit" />
   </fieldset>
+ 
 </form>
-  <script src='/js/jquery.min.js'></script>
+
+
+<script src='/js/jquery.min.js'></script>
 <script src='/js/jquery.easing.min.js'></script>
 
     <script src="/js/indexFormulaire.js"></script>
