dbb9887eeb608cbd414aa33c4e63153c3c1bc9fe
FifiTta20 <feryelbklhacene2000@gmail.com>
2022-03-21 21:44:01 +0100

    change_Feryel

--- laravel/resources/views/vue.blade.php
+++ laravel/resources/views/vue.blade.php
@@ -29,24 +29,41 @@
     <h2 class="fs-title">Identification</h2>
     <!--h3 class="fs-subtitle">This is step 1</h3-->
 
-    <p> <label>Numéro: </label>  <input type="text" id="fname" name="fname" placeholder="" onblur="validate(1)"/> </p>
-    <p> <label>Agence: </label>  <input type="text" id="lname" name="agence" placeholder="" onblur="validate(2)"/> </p>
-    <p> <label>Agence: </label>  <select name="pays">
-            <option value="France" selected="">France </option>
-            <option value="Algérie" selected="selected">Algérie </option>
-            <option value="Algérie" selected="">Adr </option>
+    <p> <label>Numéro:   <span class="text-danger"> * </span> </label>  
+        <input type="text" id="fname" name="fname" placeholder="" onblur="validate(1)"/> </p>
+
+    <p> <label>Agence:   <span class="text-danger"> * </span></label>  
+          <select id="lname" name="agence" placeholder="" onblur="validate(2)">
+            <option value="Agence1" selected="">Agence1 </option>
+            <option value="Agence2" selected="selected">Agence2 </option>
+            <option value="Agence3" selected="">Agence3</option>
           </select> </p>
-    <p> <label>Nom: </label>     <input type="text" /> </p>
-    <p> <label>Prénom: </label>   <input type="text" /> </p>
+
+    <p> <label>Nom: <span class="text-danger"> * </span> </label>     
+          <input type="text" id="nom" name="nom" placeholder="" onblur="validate(3)"/> </p>
+
+    <p> <label>Prénom: <span class="text-danger"> * </span> </label>   
+          <input type="text" id="prenom" name="prenom" placeholder="" onblur="validate(4)"/> </p>
 
 
     </br> <h2 class="fs-title">Pièces d'identités</h2>
-    <p> <label>Type: </label>  <input type="text" /> </p>
-    <p> <label>Numéro: </label>  <input type="text" />
+
+    <p> <label>Type: <span class="text-danger"> * </span></label>  
+         <input type="text" id="type" name="type" placeholder="" onblur="validate(5)"/> </p>
+
+    <p> <label>Numéro: <span class="text-danger"> * </span> </label>  
+        <input type="text" id="ans" name="numero" placeholder="" onblur="validate(6)"/>
+
         <label>Validité: </label>     <input type="date" />  </p>
     <p> <label>Délivrée à: </label>     <input type="text" />
         <label>Le: </label>     <input type="text" /> </p>
 
+</br>
+
+    <p> <label>Passeport  numéro: </label>  <input type="text" />
+        <label>Validité: </label>     <input type="date" />  </p>
+    <p> <label>Délivrée à: </label>     <input type="text" />
+        <label>Le: </label>     <input type="text" /> </p>
 
 
 
