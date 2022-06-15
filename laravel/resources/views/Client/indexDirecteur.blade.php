2f7513ef074c81e824a9fee2426ec69e845dc6d6
Hafida20-bo <bouchachia.hafida.com>
2022-06-15 19:15:23 +0100

    15/6

--- laravel/resources/views/Client/indexDirecteur.blade.php
+++ laravel/resources/views/Client/indexDirecteur.blade.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
+  <!-- <meta http-equiv="Content-Language" content="fr" /> -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
