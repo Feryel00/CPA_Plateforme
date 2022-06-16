243bc2993f3ff52aa0c31afa869e1e3914cf6338
FifiTta20 <feryelbklhacene2000@gmail.com>
2022-06-16 02:59:05 +0100

    couleurs dashboard

--- laravel/resources/views/user/show.blade.php
+++ laravel/resources/views/user/show.blade.php
@@ -7,29 +7,29 @@ body {font-family: Arial, Helvetica, sans-serif;}
 
 .container1 {
   padding: 20px;
-  background-color: #f1f1f1;
+  background-color: #AED6F1;
 }
 
 
 </style>
 <body>
-
+<title>Fiche employé</title>
 <h2>Fiche employé</h2>
 
 
-  <div class="container1">
-    <h2>Voir les Information du {{ $user->name }}</h2>
+  <div class="container1" >
+    <h2>Voir les Informations du {{ $user->name }}</h2>
     <table >
         <tr><td><div class="col-xs-12 col-sm-12 col-md-12">
 
             <div class="form-group">
-                <strong>Nom*</strong>
+                <strong><b>Nom*: <b></strong>
                 <strong>{{ $user->name }}</strong>
             </div>
         </div></td>
         <tr><td><div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Email</strong>
+                <strong>Email: </strong>
                 <strong>{{ $user->email }}</strong>
             </div>
         </div></td>
@@ -42,14 +42,14 @@ body {font-family: Arial, Helvetica, sans-serif;}
 
         <tr><td><div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Role</strong>
+                <strong>Rôle</strong>
                 @if($user->role =='1')
                 <strong> Directeur</strong>
                 @elseif($user->role =='2')
                 <strong> Charge clientele</strong>
                 @elseif($user->role =='3')
                 <strong> Charge caisse</strong>
-                @elseif($user->role =='4')
+                @elseif($user->rôle =='4')
                 <strong> Charge credit</strong>
                 @endif
             </div>
@@ -64,7 +64,7 @@ body {font-family: Arial, Helvetica, sans-serif;}
 
     </table>
 
-  <div class="container1" style="background-color:red">
+  <div class="container1" style="background-color:#3498DB">
 
 
 
